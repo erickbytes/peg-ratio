e7fcd2f484bef1f6ea04dbcd170e8c7e1a34a32d
erickbytes <erickrumbold@gmail.com>
2023-12-11 22:39:17 -0600

    added exception handling, company name and trailing EPS

--- peg_ratio.py
+++ peg_ratio.py
@@ -1,4 +1,6 @@
+import pprint
 import yfinance as yf
+
 
 def peg_ratio(ticker):
     """Returns the PEG ratio of a stock.
@@ -9,14 +11,22 @@ def peg_ratio(ticker):
     The Trailing P/E Ratio is calculated by dividing a company's
     current share price by its most recent reported earnings per share (EPS)."""
     stock_info = yf.Ticker(ticker).info
-    pe_ratio = stock_info["trailingPE"]
-    eps_growth_rate = stock_info["earningsQuarterlyGrowth"]
+    pprint.pprint(stock_info)
+    # pe_ratio = stock_info["trailingPE"]
+    # eps_growth_rate = stock_info["earningsQuarterlyGrowth"]
     # peg_ratio = pe_ratio / eps_growth_rate
+    # print(f"PE: {pe_ratio}\nEPS Growth: {eps_growth_rate}")
     peg_ratio = stock_info["pegRatio"]
-    print(f"PE: {pe_ratio}\nEPS Growth: {eps_growth_rate}")
-    return peg_ratio
+    trailing_peg_ratio = stock_info["trailingPegRatio"]
+    company_name = stock_info["shortName"]
+    return peg_ratio, trailing_peg_ratio, company_name
+
 
 # Test PEGs against: https://www.nasdaq.com/market-activity/stocks/crox/price-earnings-peg-ratios
 ticker = input("Enter the ticker symbol of the stock: ")
-peg_ratio = peg_ratio(ticker)
-print(f"{ticker} PEG ratio: {peg_ratio}")
+try:
+    peg_ratio, trailing_peg_ratio, company_name = peg_ratio(ticker)
+    print(f"{company_name} PEG ratio: {peg_ratio}")
+    print(f"{company_name} Trailing PEG ratio: {trailing_peg_ratio}")
+except KeyError:
+    print("Oops! Couldn't find PEG ratio.")
