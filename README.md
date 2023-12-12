a033c25b878fa8b7557b24ac24682d9f413a4d0c
erickbytes <erickrumbold@gmail.com>
2023-12-11 18:08:13 -0600

    added script and screenshots

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # peg-ratio
 python + yfinance peg ratio calculation generated with Bing
+
+![PEG Ratio Prompt](bing-prompt.png "Prompting Bing for a PEG ratio Python script")
+
+![Console View](console-view.png "running the Python script")
+
+**Install yfinance**
+```
+pip install yfinance
+```
---
+++ peg_ratio.py
@@ -0,0 +1,15 @@
+import yfinance as yf
+
+def get_peg_ratio(ticker):
+    stock_info = yf.Ticker(ticker).info
+    pe_ratio = stock_info["trailingPE"]
+    eps_growth_rate = stock_info["earningsQuarterlyGrowth"]
+    peg_ratio = pe_ratio / eps_growth_rate
+    print(f"PE: {pe_ratio}")
+    print(f"EPS Growth: {eps_growth_rate}")
+    print(f"PEG Ratio: {peg_ratio}")
+    return peg_ratio
+
+ticker = input("Enter the ticker symbol of the stock: ")
+peg_ratio = get_peg_ratio(ticker)
+print(f"The PEG ratio of {ticker} is {peg_ratio:.2f}.")
