a977d9d787831a839a1693e4ca4342070ba9c5e1
erickbytes <erickrumbold@gmail.com>
2023-12-11 22:08:20 -0600

    updated script to use 'pegRatio' API vlaue

--- peg_ratio.py
+++ peg_ratio.py
@@ -1,17 +1,21 @@
 import yfinance as yf
 
-def get_peg_ratio(ticker):
-    """
-    trailingPE
+def peg_ratio(ticker):
+    """Returns the PEG ratio of a stock.
+    The 'PEG ratio' (price/earnings to growth ratio) is a valuation metric
+    for determining the relative trade-off between the price of a stock,
+    the earnings generated per share (EPS), and the company's expected growth
+
     The Trailing P/E Ratio is calculated by dividing a company's
     current share price by its most recent reported earnings per share (EPS)."""
     stock_info = yf.Ticker(ticker).info
     pe_ratio = stock_info["trailingPE"]
-    eps_growth_rate = stock_info["earningsQuarterlyGrowth"]
-    peg_ratio = pe_ratio / eps_growth_rate
+    eptss_growth_rate = stock_info["earningsQuarterlyGrowth"]
+    peg_ratio = stock_info["pegRatio"]
     print(f"PE: {pe_ratio}\nEPS Growth: {eps_growth_rate}")
     return peg_ratio
 
+# Test PEGs against: https://www.nasdaq.com/market-activity/stocks/crox/price-earnings-peg-ratios
 ticker = input("Enter the ticker symbol of the stock: ")
-peg_ratio = get_peg_ratio(ticker)
-print(f"The PEG ratio of {ticker} is {peg_ratio/100:.3f}.")
+peg_ratio = peg_ratio(ticker)
+print(f"{ticker} PEG ratio: {peg_ratio}")
